a4c7b67c957c35398c205a592e586689cac0c391
ramesh349 <rameshmekala212@gmail.com>
2025-08-08 14:20:34 +0530

    Jenkins web app

---
+++ JenkinsProj-WebApp/src/main/java/com/nt/servlet/WishMessageServlet.java
@@ -0,0 +1,32 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/wishurl")
+public class WishMessageServlet extends HttpServlet {
+@Override
+protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	
+	res.setContentType("text/html");
+	
+	PrintWriter pw=res.getWriter();
+	
+	pw.println("<h1 style='color:red;text-align:center'> Good Morning </h1>");
+	
+	pw.println("<br><br><a href='index.jsp'>Home</a>");
+	
+	pw.close();
+}
+
+@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+}
---
+++ JenkinsProj-WebApp/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+
+<h1 style="color: red;text-align: center">Jenkins Web application</h1>
+
+<br><br>
+
+<a href="wishurl">Show wish message</a>
